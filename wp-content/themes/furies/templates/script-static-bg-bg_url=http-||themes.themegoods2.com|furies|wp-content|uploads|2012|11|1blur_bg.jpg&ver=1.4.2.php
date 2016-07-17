0e358c5d15956f44e09abcad33e9aff4f981fc17
shajjadhossain <shajjadhossain81@gmail.com>
2016-07-17 15:28:29 +0600

    lyricsite demo create

--- wp-content/themes/furies/templates/script-static-bg-bg_url=http-||themes.themegoods2.com|furies|wp-content|uploads|2012|11|1blur_bg.jpg&ver=1.4.2.php
+++ wp-content/themes/furies/templates/script-static-bg-bg_url=http-||themes.themegoods2.com|furies|wp-content|uploads|2012|11|1blur_bg.jpg&ver=1.4.2.php
@@ -1,2 +1,2 @@
 <!--jQuery.backstretch( "http://themes.themegoods2.com/furies/wp-content/uploads/2012/11/1blur_bg.jpg", {speed: 'slow'} );-->
-jQuery.backstretch( "http://localhost/lyricsite/wp-content/uploads/images/image-1.jpg", {speed: 'slow'} );
+jQuery.backstretch( "../lyricsite/wp-content/uploads/images/image-1.jpg", {speed: 'slow'} );
